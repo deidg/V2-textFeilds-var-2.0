5138f83d178115ee2bd93a1a2a7fae7c658ece02
deidg <64797316+deidg@users.noreply.github.com>
2023-02-22 18:18:54 +0300

    Initial Commit

---
+++ V2 textFeilds var 2.0/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  V2 textFeilds var 2.0
+//
+//  Created by Alex on 22.02.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ V2 textFeilds var 2.0UITests/V2_textFeilds_var_2_0UITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  V2_textFeilds_var_2_0UITestsLaunchTests.swift
+//  V2 textFeilds var 2.0UITests
+//
+//  Created by Alex on 22.02.2023.
+//
+
+import XCTest
+
+final class V2_textFeilds_var_2_0UITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
