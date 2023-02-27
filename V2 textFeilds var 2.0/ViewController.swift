ed3d918db265053135b45525cdea84ec853484b7
deidg <64797316+deidg@users.noreply.github.com>
2023-02-27 19:54:33 +0300

    commit 1.7.13  //cleaned.  before scroll and keyboard appearance

--- V2 textFeilds var 2.0/ViewController.swift
+++ V2 textFeilds var 2.0/ViewController.swift
@@ -7,9 +7,6 @@
 //  Created by Alex on 07.02.2023.
 //
 
-//в этом проекте который скачал - посмотреть еще раз как он это делал и повторить. кажется подходит.
-//ЛИБО можно сделать так - счётчик идёт до 8 и после отображается цветом валидный пароль или нет.
-
 
 import UIKit
 import SnapKit
@@ -199,7 +196,6 @@ final class ViewController: UIViewController {
     
     let validationMinLengthLabel: UILabel = {
         let validationMinLengthLabel = UILabel()
-        //        validationMinLengthLabel.backgroundColor = .purple
         validationMinLengthLabel.text = Constants.LabelsTexts.validationMinLengthLabelText
         validationMinLengthLabel.textColor = Constants.LabelsTexts.smallLabelTextColor
         validationMinLengthLabel.font = Constants.LabelsFonts.smallLabelFont
@@ -210,7 +206,6 @@ final class ViewController: UIViewController {
         validationDigitLabel.text = Constants.LabelsTexts.validationDigitLabelText
         validationDigitLabel.textColor = Constants.LabelsTexts.smallLabelTextColor
         validationDigitLabel.font = Constants.LabelsFonts.smallLabelFont
-        //        validationDigitLabel.backgroundColor = .red
         return validationDigitLabel
     }()
     let validationLowerCaseLabel: UILabel = {
@@ -218,7 +213,6 @@ final class ViewController: UIViewController {
         validationLowerCaseLabel.text = Constants.LabelsTexts.validationLowerCaseLabelText
         validationLowerCaseLabel.textColor = Constants.LabelsTexts.smallLabelTextColor
         validationLowerCaseLabel.font = Constants.LabelsFonts.smallLabelFont
-        //        validationLowerCaseLabel.backgroundColor = .green
         return validationLowerCaseLabel
     }()
     let validationCapitalCaseLabel: UILabel = {
@@ -226,14 +220,9 @@ final class ViewController: UIViewController {
         validationCapitalCaseLabel.text = Constants.LabelsTexts.validationCapitalCaseLabelText
         validationCapitalCaseLabel.textColor = Constants.LabelsTexts.smallLabelTextColor
         validationCapitalCaseLabel.font = Constants.LabelsFonts.smallLabelFont
-        //        validationCapitalCaseLabel.backgroundColor = .link
         return validationCapitalCaseLabel
     }()
-    
-    
-    
-    
-    
+   
     private func setupItemsOnView() {
         
         view.addSubview(scrollView)
@@ -404,9 +393,6 @@ final class ViewController: UIViewController {
             make.trailing.equalToSuperview().inset(180)
             make.height.equalTo(15)
         }
-        
-        
-        
     }
     
     func defaultConfiguration() {
@@ -416,16 +402,7 @@ final class ViewController: UIViewController {
         self.view.addGestureRecognizer(tapGestureRecognizer)
         NotificationCenter.default.addObserver(self, selector: #selector(ViewController.keyboardWillShow), name: UIResponder.keyboardWillShowNotification, object: nil)
         NotificationCenter.default.addObserver(self, selector: #selector(ViewController.keyboardWillHide), name: UIResponder.keyboardWillHideNotification, object: nil)
-        
-        //!!!код для выравнивания текста в лейбле!!!!
-        //        let paragraph = NSMutableParagraphStyle()
-        //        paragraph.lineSpacing = 7.0
-        //        let attributedString = NSMutableAttributedString(string: "Min length 8 characters.\nMin 1 digit,\nMin 1 lowercase,\nMin 1 capital required.\n")
-        //        attributedString.addAttribute(NSAttributedString.Key.paragraphStyle, value: paragraph, range: NSMakeRange(0, attributedString.length))
-        //        validationRulesLabel.attributedText = attributedString
     }
-    
-    
     
     func containsDigit(_ value: String) -> Bool {
         let reqularExpression = ".*[0-9]+.*"  //"^([0-9])$"
@@ -447,9 +424,6 @@ final class ViewController: UIViewController {
         let predicate = NSPredicate(format: "SELF MATCHES %@", reqularExpression)
         return predicate.evaluate(with: value)
     }
-    
-    
-    
     
     @objc func keyboardWillShow(notification: NSNotification) {
         guard let keyboardSize = (notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue)?.cgRectValue else {
@@ -473,9 +447,6 @@ final class ViewController: UIViewController {
     @objc func backgroundTap(_ sender: UITapGestureRecognizer) {
         self.view.endEditing(true)
     }
-    
-    
-    
 }
 
 extension String {
@@ -507,45 +478,14 @@ extension ViewController: UITextFieldDelegate {
                 charactersCounter.text = "10/10"
             }
             return lengthToAdd <= 10
-            } else if textField == linkTextField {
-//
-//                        let link = linkTextField.text ?? ""
-//
-//                        func isLinkValid(_ link : String) -> Bool {
-//                            let linkTest = NSPredicate(format: "SELF MATCHES %@", linkRegex)
-//                            return linkTest.evaluate(with: link)
-//                        }
-//                        print(isLinkValid(link))
-//
-//
-//                        let delay : Double = 5.0
-//                        DispatchQueue.main.asyncAfter(deadline: .now() + delay) {
-//                            func startBrowser(_ sender: Any) {
-//                                if let urlString = self.linkTextField.text {
-//                                    let url: URL?
-//                                    if urlString.hasPrefix("http://") {
-//                                        url = URL(string: urlString)
-//                                    } else {
-//                                        url = URL(string: "http://" + urlString)
-//                                    }
-//                                    if let url = url {
-//                                        let sfViewController = SFSafariViewController(url: url)
-//                                        self.present(sfViewController, animated: true, completion: nil)
-//                                        print ("Now browsing in SFSafariViewController")
-//                                    }
-//                                }
-//                            }
-//                            startBrowser(self.linkTextField)
-//                        }
-            //============
+        } else if textField == linkTextField {
+            //
         } else if textField == passwordTextField {
-            
-//            var counter: Int = 0
-            
+   
             if let text = passwordTextField.text,
                let textRange = Range(range, in: text) {
                 let updatedText = text.replacingCharacters(in: textRange, with: string)
-
+                
                 
                 let decimalCharacters = CharacterSet.decimalDigits
                 let decimalRange = updatedText.rangeOfCharacter(from: decimalCharacters)
@@ -587,29 +527,10 @@ extension ViewController: UITextFieldDelegate {
         }
         return true
     }
-//}
-
-
-
-//рабочий код для ТФ5
-//            let password = passwordTextField.text ?? ""
-//            func isPasswordValid(_ password : String) -> Bool {
-//                let passwordTest = NSPredicate(format: "SELF MATCHES %@", passwordRegex)
-//                return passwordTest.evaluate(with: password)
-//            }
-//            if isPasswordValid(password) == true {
-//                validationRulesLabel.textColor = .green
-//            } else {
-//                validationRulesLabel.textColor = .red
-//            }
-//        }
-//        return true
-//    }
-
-//TODO: проверить варианты ссылки с www.  и без.  ВАЛИДНЫЙ КОД НИЖЕ для ТФ4
+    
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
         if textField == linkTextField {
-
+            
             let link = linkTextField.text ?? ""
             func isLinkValid(_ link : String) -> Bool {
                 let linkTest = NSPredicate(format: "SELF MATCHES %@", linkRegex)
@@ -617,7 +538,7 @@ extension ViewController: UITextFieldDelegate {
             }
             if isLinkValid(link) {
                 print("its valid LINK")
-
+                
                 let delay : Double = 2.0 // 5.0
                 DispatchQueue.main.asyncAfter(deadline: .now() + delay) {
                     func startBrowser(_ sender: Any) {
@@ -643,63 +564,44 @@ extension ViewController: UITextFieldDelegate {
         }
         return true
     }
-
-
-
-//MARK: constants
-enum Constants {
-    enum LabelsSettings {
-        static let lettersTextViewCornerRadius: CGFloat = 10
-    }
-    enum LabelsFonts {
-        static let mainLabelFont = UIFont(name: "Rubik", size: 34)
-        static let smallLabelFont = UIFont(name: "Rubik", size: 13)
-    }
-    enum LabelsTexts {
-        static let mainTitleLabeText = "Text Fields"
-        static let noDigitLabelText = "NO digit field"
-        static let inputLimitLabelText = "Input limit"
-        static let charactersCounterText = "0/10"
-        static let onlyCharectersLabelText = "Only characters"
-        static let linkLabelText = "Link"
-        static let validationLabelText = "Validation rules"
-        //            static let validationRulesLabelText = "Min length 8 characters,\nMin 1 digit,\nMin 1 lowercase,\nMin 1 capital required.\n"
-        
-        static let validationMinLengthLabelText = "Min length 8 characters,"
-        static let validationDigitLabelText = "Min 1 digit,"
-        static let validationLowerCaseLabelText = "Min 1 lowercase,"
-        static let validationCapitalCaseLabelText = "Min 1 capital required.\n"
-        
-        
-        
-        
-        static let smallLabelTextColor = UIColor(red: 45/255, green: 45/255, blue: 45/255, alpha: 1)
-        static let validationRulesLabelTextColor = UIColor(red: 87/255, green: 87/255, blue: 87/255, alpha: 1)
-    }
-    enum LabelsBackgroundColors {
-        static let labelBackgoundColors = UIColor.white
-    }
-    enum TextFields {
-        static let lettersTextFieldPlaceholderText = "Type here"
-        static let onlyCharectersLabelPlaceholderText = "wwwww-ddddd"
-        static let linkTextFieldPlaceholderText = "www.example.com"
-        static let passwordTextFieldPlaceholderText = "Password"
-        static let textFieldFont = UIFont(name: "Rubik", size: 17)
-        static let textFieldTextColor = UIColor(red: 60/255, green: 60/255, blue: 67/255, alpha: 0.6)
-        static let viewBackgroundColor = UIColor(red: 118/255, green: 118/255, blue: 128/255, alpha: 0.12)
-        static let textFieldBackgroundColor = UIColor(red: 118/255, green: 118/255, blue: 128/255, alpha: 0.12)
+    
+    //MARK: constants
+    enum Constants {
+        enum LabelsSettings {
+            static let lettersTextViewCornerRadius: CGFloat = 10
+        }
+        enum LabelsFonts {
+            static let mainLabelFont = UIFont(name: "Rubik", size: 34)
+            static let smallLabelFont = UIFont(name: "Rubik", size: 13)
+        }
+        enum LabelsTexts {
+            static let mainTitleLabeText = "Text Fields"
+            static let noDigitLabelText = "NO digit field"
+            static let inputLimitLabelText = "Input limit"
+            static let charactersCounterText = "0/10"
+            static let onlyCharectersLabelText = "Only characters"
+            static let linkLabelText = "Link"
+            static let validationLabelText = "Validation rules"
+            static let validationMinLengthLabelText = "Min length 8 characters,"
+            static let validationDigitLabelText = "Min 1 digit,"
+            static let validationLowerCaseLabelText = "Min 1 lowercase,"
+            static let validationCapitalCaseLabelText = "Min 1 capital required.\n"
+            static let smallLabelTextColor = UIColor(red: 45/255, green: 45/255, blue: 45/255, alpha: 1)
+            static let validationRulesLabelTextColor = UIColor(red: 87/255, green: 87/255, blue: 87/255, alpha: 1)
+        }
+        enum LabelsBackgroundColors {
+            static let labelBackgoundColors = UIColor.white
+        }
+        enum TextFields {
+            static let lettersTextFieldPlaceholderText = "Type here"
+            static let onlyCharectersLabelPlaceholderText = "wwwww-ddddd"
+            static let linkTextFieldPlaceholderText = "www.example.com"
+            static let passwordTextFieldPlaceholderText = "Password"
+            static let textFieldFont = UIFont(name: "Rubik", size: 17)
+            static let textFieldTextColor = UIColor(red: 60/255, green: 60/255, blue: 67/255, alpha: 0.6)
+            static let viewBackgroundColor = UIColor(red: 118/255, green: 118/255, blue: 128/255, alpha: 0.12)
+            static let textFieldBackgroundColor = UIColor(red: 118/255, green: 118/255, blue: 128/255, alpha: 0.12)
+        }
     }
 }
-
-//MARK: keyboard    - валидный код ниже
-//    func textFieldDidBeginEditing(_ textField: UITextField) {
-//        self.activeTextField = textField
-//    }
-//    func textFieldDidEndEditing(_ textField: UITextField) {
-//        self.activeTextField = nil
-//    }
-//}
-
-
-    }
 
